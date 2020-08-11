b14b8c96a4fecf917166cba058bed0373e208a7a
Džiugas Juozapaitis <dziugasj@gmail.com>
2020-08-11 11:49:19 +0300

    Generated by Spring initializr

---
+++ src/test/java/io/github/dziugasj/selenidejunit5sandbox/SelenideJunit5SandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dziugasj.selenidejunit5sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelenideJunit5SandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
